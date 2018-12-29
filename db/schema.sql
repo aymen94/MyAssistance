4ed08255179ee662ee760eca225fc9f1ae0c320a
Aymen Naghmouchi <aymenadvance@gmail.com>
2018-12-29 18:06:31 +0100

    added auto_increment
    Signed-off-by: Aymen Naghmouchi <aymenadvance@gmail.com>

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA my_assistance;
 
 CREATE TABLE my_assistance.utente
 (
-  id               INT PRIMARY KEY,
+  id               INT PRIMARY KEY AUTO_INCREMENT,
   username         VARCHAR(20) UNIQUE NOT NULL,
   `password`       VARCHAR(100)       NOT NULL,
   email            VARCHAR(80) UNIQUE NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE my_assistance.utente
 
 CREATE TABLE my_assistance.ufficio_tecnico
 (
-  id           INT PRIMARY KEY,
+  id           INT PRIMARY KEY AUTO_INCREMENT,
   nome         VARCHAR(50) UNIQUE NOT NULL,
   tel          CHAR(15),
   email        VARCHAR(55) UNIQUE NOT NULL,
@@ -24,14 +24,14 @@ CREATE TABLE my_assistance.ufficio_tecnico
 
 CREATE TABLE my_assistance.tipologia
 (
-  id       INT PRIMARY KEY,
+  id       INT PRIMARY KEY AUTO_INCREMENT,
   nome     VARCHAR(20) UNIQUE NOT NULL,
   priorita TINYINT            NOT NULL --  da 1 (priorità bassa) a 10 (priorità alta)
 );
 
 CREATE TABLE my_assistance.segnalazione
 (
-  cod                 INT PRIMARY KEY,
+  cod                 INT PRIMARY KEY AUTO_INCREMENT,
   titolo              VARCHAR(50) NOT NULL,
   descrizione         TEXT        NOT NULL,
   stato               TINYINT     NOT NULL, -- -1 = rifiutata, 0 = aperta, 1 = assegnata, 2 = risolta
