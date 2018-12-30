deb95d73634b6451b94e77d12959948122def703
Aymen Naghmouchi <aymenadvance@gmail.com>
2018-12-30 16:59:38 +0100

    chenge param 'password' in 'pass'
    Signed-off-by: Aymen Naghmouchi <aymenadvance@gmail.com>

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE my_assistance.utente
 (
   id               INT PRIMARY KEY AUTO_INCREMENT,
   username         VARCHAR(20) UNIQUE NOT NULL,
-  `password`       VARCHAR(100)       NOT NULL,
+  pass             VARCHAR(100)       NOT NULL,
   email            VARCHAR(80) UNIQUE NOT NULL,
   nome             VARCHAR(50)        NOT NULL,
   cognome          VARCHAR(50)        NOT NULL,
