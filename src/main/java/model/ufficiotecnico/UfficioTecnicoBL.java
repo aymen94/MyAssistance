993c8e2d946604d403fad2b5a73046dcea10c5e0
Andrea Mennillo <handymenny@outlook.com>
2019-01-05 21:50:56 +0100

    UfficioTecnicoBL: Add memorizzaUfficioTecnico method
    The method is not implemented

--- src/main/java/model/ufficiotecnico/UfficioTecnicoBL.java
+++ src/main/java/model/ufficiotecnico/UfficioTecnicoBL.java
@@ -9,4 +9,17 @@ package model.ufficiotecnico;
  * The Class UfficioTecnicoBL.
  */
 public class UfficioTecnicoBL {
+
+    /**
+     * Memorizza ufficio tecnico.
+     *
+     * @param nome       the nome
+     * @param tel        the tel
+     * @param ubicazione the ubicazione
+     * @return true, if successful
+     */
+    public boolean memorizzaUfficioTecnico(final String nome, final String tel,
+            final String ubicazione) {
+        return false;
+    }
 }
