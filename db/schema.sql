99b6a0833e487fa59f1727e448fdeba9d5b06935
Andrea Mennillo <handymenny@outlook.com>
2019-01-06 15:24:50 +0100

    Revert "DB: Add data_di_nascita field"
    This reverts commit 7bde0b44c7e6fa7907d7d2f781414b7d0c18ba47.

--- db/schema.sql
+++ db/schema.sql
@@ -9,7 +9,6 @@ CREATE TABLE my_assistance.utente
   nome             VARCHAR(50)        NOT NULL,
   cognome          VARCHAR(50)        NOT NULL,
   sesso            TINYINT            NOT NULL, -- 0 = Altro, 1 = Maschio, 2 = Femmina
-  data_di_nascita  DATE               NOT NULL,
   data_sospensione DATE,
   is_gestore       BOOLEAN DEFAULT FALSE
 );
