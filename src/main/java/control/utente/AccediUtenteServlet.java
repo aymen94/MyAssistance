ce35cf4b03e9ba4d6a8ef2e87a6f221e3d855e88
Aymen Naghmouchi <aymenadvance@gmail.com>
2019-01-06 13:15:02 +0100

    servlet structure with javadoc

---
+++ src/main/java/control/utente/AccediUtenteServlet.java
@@ -0,0 +1,35 @@
+/*
+  Project: MyAssistance
+  Author: TeamC
+  Date: 05/01/2019
+*/
+package control.utente;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet per accesso utente(CSU).
+ */
+public class AccediUtenteServlet extends HttpServlet {
+    /**
+     *
+     */
+    @Override public void doGet(final HttpServletRequest req,
+            final HttpServletResponse resp)
+            throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    /**
+     *
+     */
+    @Override public void doPost(final HttpServletRequest req,
+            final HttpServletResponse resp)
+            throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
