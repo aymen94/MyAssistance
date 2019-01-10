30eebd21782706cf363d722376bd56d14a49e5f2
Andrea Mennillo <handymenny@outlook.com>
2019-01-10 01:19:21 +0100

    SegnalazioneBL: Implement insertSegnalazione, updateSegnalazione, rifiutaSegnalazione

--- src/main/java/model/segnalazione/SegnalazioneBL.java
+++ src/main/java/model/segnalazione/SegnalazioneBL.java
@@ -5,16 +5,23 @@ Date: 23/12/2018
 */
 package model.segnalazione;
 
+import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.List;
 
-import model.ufficiotecnico.UfficioTecnico;
 import model.utente.Utente;
+import model.utente.UtenteDB;
 
 // TODO: Auto-generated Javadoc
 /**
  * The Class SegnalazioneBL.
  */
 public final class SegnalazioneBL {
+
+    /**
+     * The Constant MAX_TITLE_LENGTH.
+     */
+    private static final int MAX_TITLE_LENGTH = 50;
 
     /**
      * This is an utility class. So no constructor should be used.
@@ -35,8 +42,26 @@ public final class SegnalazioneBL {
     public static boolean insertSegnalazione(final String aTitolo,
             final String aDescrizione, final int aIdTipologia,
             final int aIdAutore) {
+        try {
+            final Tipologia aTipologia = TipologiaDB.getById(aIdTipologia);
+            final Utente aAutore = UtenteDB.getById(aIdAutore);
+            if (aTitolo.length() > 0 && aTitolo.length() <= MAX_TITLE_LENGTH
+                    && aDescrizione.length() > 0 && aTipologia != null
+                    && aAutore != null) {
+                final Segnalazione aSegnalazione = new Segnalazione();
+                aSegnalazione.setTitolo(aTitolo);
+                aSegnalazione.setDescrizione(aDescrizione);
+                aSegnalazione.setTipologia(aTipologia);
+                aSegnalazione.setAutore(aAutore);
+                // Set the current date
+                aSegnalazione.setDataSegnalazione(
+                        java.sql.Date.valueOf(LocalDate.now()));
+                return SegnalazioneDB.insert(aSegnalazione) > 0;
+            }
+        } catch (final SQLException e) {
+            e.printStackTrace();
+        }
         return false;
-
     }
 
     /**
@@ -55,13 +80,30 @@ public final class SegnalazioneBL {
      * Modifica segnalazione.
      *
      * @param aCod         the cod
-     * @param atitolo      the atitolo
-     * @param adescrizione the adescrizione
-     * @param aIdTipologia the atipologia
+     * @param aTitolo      the titolo
+     * @param aDescrizione the descrizione
+     * @param aIdTipologia the tipologia
      * @return true, if successful
      */
-    public static boolean updateSegnalazione(final String atitolo,
-            final String adescrizione, final int aIdTipologia, final int aCod) {
+    public static boolean updateSegnalazione(final String aTitolo,
+            final String aDescrizione, final int aIdTipologia, final int aCod) {
+        try {
+            final Tipologia aTipologia = TipologiaDB.getById(aIdTipologia);
+            final Segnalazione aSegnalazione = SegnalazioneDB.getByCod(aCod);
+
+            if (aTitolo.length() > 0 && aTitolo.length() <= MAX_TITLE_LENGTH
+                    && aDescrizione.length() > 0 && aTipologia != null
+                    && aSegnalazione != null
+                    && aSegnalazione.getStato() == Segnalazione.STATO_APERTO) {
+                aSegnalazione.setTitolo(aTitolo);
+                aSegnalazione.setDescrizione(aDescrizione);
+                aSegnalazione.setTipologia(aTipologia);
+                return SegnalazioneDB.update(aSegnalazione) > 0;
+            }
+
+        } catch (final SQLException e) {
+            e.printStackTrace();
+        }
         return false;
 
     }
@@ -108,8 +150,22 @@ public final class SegnalazioneBL {
      */
     public static boolean rifiutaSegnalazione(final int aCod,
             final String aMotivazioneRifiuto) {
-        return false;
 
+        try {
+            final Segnalazione aSegnalazione = SegnalazioneDB.getByCod(aCod);
+            if (aSegnalazione != null
+                    && aSegnalazione.getStato() == Segnalazione.STATO_APERTO
+                    && aMotivazioneRifiuto.length() > 0) {
+                aSegnalazione.setStato(Segnalazione.STATO_RIFIUTATO);
+                aSegnalazione.setMotivazioneRifiuto(aMotivazioneRifiuto);
+                return SegnalazioneDB.update(aSegnalazione) > 0;
+            }
+
+        } catch (final SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return false;
     }
 
     /**
