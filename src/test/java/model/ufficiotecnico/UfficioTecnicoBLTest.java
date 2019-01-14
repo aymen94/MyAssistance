c28fdcc983bd872671f542e203499c9a01c27d32
Gaetanosk8 <44467915+tanosk8@users.noreply.github.com>
2019-01-14 13:09:13 +0100

    UfficioTecnico - add eguals .

---
+++ src/test/java/model/ufficiotecnico/UfficioTecnicoBLTest.java
@@ -0,0 +1,29 @@
+package model.ufficiotecnico;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UfficioTecnicoBLTest {
+
+    @Before public void setUp() throws Exception {
+    }
+
+    @After public void tearDown() throws Exception {
+    }
+
+    @Test public void insertUfficioTecnico() {
+
+    }
+
+    @Test public void ottieniUfficiTecnici() {
+    }
+
+    @Test public void ottieniUfficio() {
+    }
+
+    @Test public void deleteUfficioTecnico() {
+    }
+}
