898abce3d3931faf8250e9d6b82f75bd19e73ef2
Andrea Mennillo <handymenny@outlook.com>
2019-01-25 11:22:11 +0100

    test: UfficioTecnicoDB: Add missing assert, cleanup and add javadoc

--- src/test/java/model/ufficio_tecnico/UfficioTecnicoDBTest.java
+++ src/test/java/model/ufficio_tecnico/UfficioTecnicoDBTest.java
@@ -1,11 +1,8 @@
 package model.ufficio_tecnico;
 
-import model.ufficiotecnico.UfficioTecnico;
-import model.ufficiotecnico.UfficioTecnicoBL;
-import model.ufficiotecnico.UfficioTecnicoDB;
-import model.utente.Utente;
-import org.junit.*;
-import pool.Database;
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.sql.Connection;
@@ -16,25 +13,41 @@ import java.util.List;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 
-import static junit.framework.TestCase.assertEquals;
-import static org.junit.Assert.*;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
+import model.ufficiotecnico.UfficioTecnico;
+import model.ufficiotecnico.UfficioTecnicoDB;
+import pool.Database;
+
+/**
+ * The Class UfficioTecnicoDBTest.
+ */
 public class UfficioTecnicoDBTest {
 
+    /**
+     * The file handler.
+     */
     private static FileHandler fileHandler = null;
-    private static Logger logger = null;
 
+    /**
+     * The logger.
+     */
+    private static Logger logger = null;
 
     /**
      * Sets the up class.
      *
-     * @throws SQLException the SQL exception
+     * @throws IOException Signals that an I/O exception has occurred.
      */
-    @BeforeClass public static void setUpClass()
-            throws IOException {
+    @BeforeClass
+    public static void setUpClass() throws IOException {
         Database.initializePool();
 
-        fileHandler = new FileHandler("result_test\\"+UfficioTecnicoDB.class.getName());
+        fileHandler = new FileHandler(
+                "result_test\\" + UfficioTecnicoDB.class.getName());
         logger = Logger.getLogger(UfficioTecnicoDBTest.class.getName());
         logger.addHandler(fileHandler);
         logger.config("logger loaded");
@@ -42,17 +55,26 @@ public class UfficioTecnicoDBTest {
 
     /**
      * Tear down class.
+     *
+     * @throws SQLException the SQL exception
      */
-    @AfterClass public static void tearDownClass() throws SQLException {
-        UfficioTecnicoDB utenteDBTest = new UfficioTecnicoDB();
+    @AfterClass
+    public static void tearDownClass() throws SQLException {
+        final UfficioTecnicoDB utenteDBTest = new UfficioTecnicoDB();
         utenteDBTest.deleteById(1);
         logger.info("clear db  \n output:" + utenteDBTest.deleteById(1));
         Database.destroyPool();
 
     }
 
-    @Before public void clearDb() throws SQLException {
-        Connection conn = Database.getConnection();
+    /**
+     * Clear db.
+     *
+     * @throws SQLException the SQL exception
+     */
+    @Before
+    public void clearDb() throws SQLException {
+        final Connection conn = Database.getConnection();
         conn.prepareStatement("TRUNCATE TABLE my_assistance.ufficio_tecnico")
                 .executeUpdate();
         Database.freeConnection(conn);
@@ -60,90 +82,105 @@ public class UfficioTecnicoDBTest {
     }
 
     /*
-
-     */
-    /**
      *
-     * @throws SQLException the SQL exception
-     *//*
-
-    @Test public void insert()  throws SQLException {
-        UfficioTecnicoDB ufficioTecnicoDB = new UfficioTecnicoDB();
-        UfficioTecnico test1 = new UfficioTecnico();
-        test1.setNome("nomesettato");
-        test1.setTel("1234567890");
-        test1.setEmail("abcdef@gmail.com");
-        test1.setUbicazione("Roma");
-        ufficioTecnicoDB.insert(test1);
-        logger.info("method: insert, \noutput: " + test1);
-    }
-
-*/
-
-
+     * @Test public void insert() throws SQLException { UfficioTecnicoDB
+     * ufficioTecnicoDB = new UfficioTecnicoDB(); UfficioTecnico test1 = new
+     * UfficioTecnico(); test1.setNome("nomesettato");
+     * test1.setTel("1234567890"); test1.setEmail("abcdef@gmail.com");
+     * test1.setUbicazione("Roma"); ufficioTecnicoDB.insert(test1);
+     * logger.info("method: insert, \noutput: " + test1); }
+     *
+     */
 
     /**
+     * Gets the all.
+     *
      *
      * @throws SQLException the SQL exception
      */
 
-    @Test public void getAll() throws SQLException {
-        UfficioTecnicoDB ufficioTecnicoTest = new UfficioTecnicoDB();
-        List<UfficioTecnico> list = ufficioTecnicoTest.getAll();
-        if (list == null)
+    @Test
+    public void getAll() throws SQLException {
+        final UfficioTecnicoDB ufficioTecnicoTest = new UfficioTecnicoDB();
+        final List<UfficioTecnico> list = ufficioTecnicoTest.getAll();
+        if (list == null) {
             logger.info("method: getAll()==1 \noutput:\n " + 0);
-        else
+        } else {
             logger.info("method: getAll()==1 \noutput:\n " + list.size());
+        }
     }
 
-    @Test public void testGetById1() throws SQLException {
-        UfficioTecnicoDB ufficioTecnicoTest = new UfficioTecnicoDB();
-        UfficioTecnico uff = ufficioTecnicoTest.getById(3);
+    /**
+     * Test get by id 1.
+     *
+     * @throws SQLException the SQL exception
+     */
+    @Test
+    public void testGetById1() throws SQLException {
+        final UfficioTecnicoDB ufficioTecnicoTest = new UfficioTecnicoDB();
+        final UfficioTecnico uff = ufficioTecnicoTest.getById(3);
         logger.info("method: getById(1) \noutput:" + uff);
         assertNull(uff);
     }
 
-    @Test public void testGetById2() throws SQLException {
+    /**
+     * Test get by id 2.
+     *
+     * @throws SQLException the SQL exception
+     */
+    @Test
+    public void testGetById2() throws SQLException {
 
-        Connection conn = Database.getConnection();
+        final Connection conn = Database.getConnection();
         conn.prepareStatement(
-                "INSERT INTO my_assistance.ufficio_tecnico (nome,tel,email,ubicazione)"
-                        + " VALUES ('PIO','1234567890','ABCD@GMAIL.COM','RIMINI')")
+                "INSERT INTO my_assistance.ufficio_tecnico "
+                        + "(nome,tel,email,ubicazione) VALUES "
+                        + "('PIO','1234567890','ABCD@GMAIL.COM','RIMINI')")
                 .executeUpdate();
         Database.freeConnection(conn);
-        UfficioTecnico uff2 = new UfficioTecnico();
+        final UfficioTecnico uff2 = new UfficioTecnico();
         uff2.setId(1);
         uff2.setNome("PIO");
         uff2.setTel("1234567890");
         uff2.setEmail("ABCD@GMAIL.COM");
         uff2.setUbicazione("RIMINI");
-        UfficioTecnicoDB ufficioTecnicoTest = new UfficioTecnicoDB();
-        UfficioTecnico uff = ufficioTecnicoTest.getById(1);
+        final UfficioTecnicoDB ufficioTecnicoTest = new UfficioTecnicoDB();
+        final UfficioTecnico uff = ufficioTecnicoTest.getById(1);
 
         logger.info(
                 "method: getById(1)\noutput :" + uff + " \noracolo:" + uff2);
         assertEquals(uff, uff2);
     }
 
+    /**
+     * Delete by id.
+     *
+     * @throws SQLException the SQL exception
+     */
+    @Test
+    public void deleteById() throws SQLException {
+        final UfficioTecnicoDB ufficioTecnicoTest = new UfficioTecnicoDB();
 
-    @Test public void deleteById() throws SQLException {
-        UfficioTecnicoDB ufficioTecnicoTest = new UfficioTecnicoDB();
-
-        logger.info("method: deleteById(1) \noutput:" + ufficioTecnicoTest
-                .deleteById(1));
+        logger.info("method: deleteById(1) \noutput:"
+                + ufficioTecnicoTest.deleteById(1));
     }
 
-    @Test(expected = SQLException.class) public void testInsert1()
-            throws SQLException {
-        UfficioTecnicoDB ufficioTecnicoTest = new UfficioTecnicoDB();
+    /**
+     * Test insert 1.
+     *
+     * @throws SQLException the SQL exception
+     */
+    @Test(expected = SQLException.class)
+    public void testInsert1() throws SQLException {
+        final UfficioTecnicoDB ufficioTecnicoTest = new UfficioTecnicoDB();
 
         boolean res = false;
 
-        String aNome = null;
-        String aEmail = "mail@mgail.com";
-        String aTel = "1234567890";
-        String aUbicazione = "Roma";
-        UfficioTecnico uff = new UfficioTecnico();
+        final String aNome = null;
+        final String aEmail = "mail@mgail.com";
+        final String aTel = "1234567890";
+        final String aUbicazione = "Roma";
+        final UfficioTecnico uff = new UfficioTecnico();
         uff.setNome(aNome);
         uff.setEmail(aEmail);
         uff.setTel(aTel);
@@ -151,50 +188,56 @@ public class UfficioTecnicoDBTest {
         try {
             res = ufficioTecnicoTest.insert(uff) > 0;
         } finally {
-            logger.info(
-                    "methods Insert Test 1 (empty string 'nome') : \nEsito inserimento : "
-                            + res);
-        }
-
-    }
-
-    @Test(expected = SQLException.class) public void testInsert2()
-            throws SQLException {
-        UfficioTecnicoDB ufficioTecnicoTest = new UfficioTecnicoDB();
-        boolean res = false;
-
-        String aNome = "ufficio amministrazione";
-        String aEmail = null;
-        String aTel = "1234567890";
-        String aUbicazione = "Roma";
-        UfficioTecnico uff = new UfficioTecnico();
-        uff.setNome(aNome);
-        uff.setEmail(aEmail);
-        uff.setTel(aTel);
-        uff.setUbicazione(aUbicazione);
-        try {
-            res = ufficioTecnicoTest.insert(uff) > 0;
-        } finally {
-            logger.info(
-                    "methods Insert Test 2 (empty string 'email') : \nEsito inserimento : "
-                            + res);
+            logger.info("methods Insert Test 1 (empty string 'nome') : "
+                    + "\nEsito inserimento : " + res);
         }
 
     }
 
     /**
+     * Test insert 2.
+     *
+     * @throws SQLException the SQL exception
+     */
+    @Test(expected = SQLException.class)
+    public void testInsert2() throws SQLException {
+        final UfficioTecnicoDB ufficioTecnicoTest = new UfficioTecnicoDB();
+        boolean res = false;
+
+        final String aNome = "ufficio amministrazione";
+        final String aEmail = null;
+        final String aTel = "1234567890";
+        final String aUbicazione = "Roma";
+        final UfficioTecnico uff = new UfficioTecnico();
+        uff.setNome(aNome);
+        uff.setEmail(aEmail);
+        uff.setTel(aTel);
+        uff.setUbicazione(aUbicazione);
+        try {
+            res = ufficioTecnicoTest.insert(uff) > 0;
+        } finally {
+            logger.info("methods Insert Test 2 (empty string 'email') : "
+                    + "\nEsito inserimento : " + res);
+        }
+
+    }
+
+    /**
+     * Test insert 3.
+     *
      * @throws SQLException sql error
      */
 
-    @Test public void testInsert3() throws SQLException {
-        UfficioTecnicoDB ufficioTecnicoTest = new UfficioTecnicoDB();
+    @Test
+    public void testInsert3() throws SQLException {
+        final UfficioTecnicoDB ufficioTecnicoTest = new UfficioTecnicoDB();
         boolean res = false;
 
-        String aNome = "ufficio amministrazione";
-        String aEmail = "abcde@gmail.com";
-        String aTel = null;
-        String aUbicazione = "Roma";
-        UfficioTecnico uff = new UfficioTecnico();
+        final String aNome = "ufficio amministrazione";
+        final String aEmail = "abcde@gmail.com";
+        final String aTel = null;
+        final String aUbicazione = "Roma";
+        final UfficioTecnico uff = new UfficioTecnico();
         uff.setNome(aNome);
         uff.setEmail(aEmail);
         uff.setTel(aTel);
@@ -206,52 +249,66 @@ public class UfficioTecnicoDBTest {
             assertTrue(res);
             assertEquals(uff, nuovoUfficioTecnico);
         } finally {
-            logger.info(
-                    "methods Insert Test 3 (empty string 'tel') : \nEsito inserimento : "
-                            + res);
+            logger.info("methods Insert Test 3 (empty string 'tel') :"
+                    + "\nEsito inserimento : " + res);
 
         }
     }
 
-    @Test public void getAllTest() throws SQLException {
-        boolean res = false;
-        String aNome1 = "nome1", aNome2 = "nome2";
-        String aEmail1 = "abcde@gmail.com", aEmail2 = "abc@gmail.com";
-        String aTel1 = "0123456789", aTel2 = "1234567890";
-        String aUbicazione1 = "Roma", aUbicazione2 = "Berlino";
-        UfficioTecnicoDB ufficioTecnicoDBTest = new UfficioTecnicoDB();
+    /**
+     * Gets the all test.
+     *
+     *
+     * @throws SQLException the SQL exception
+     */
+    @Test
+    public void getAllTest() throws SQLException {
+        final String aNome1 = "nome1", aNome2 = "nome2";
+        final String aEmail1 = "abcde@gmail.com", aEmail2 = "abc@gmail.com";
+        final String aTel1 = "0123456789", aTel2 = "1234567890";
+        final String aUbicazione1 = "Roma", aUbicazione2 = "Berlino";
+        final UfficioTecnicoDB ufficioTecnicoDBTest = new UfficioTecnicoDB();
 
-        UfficioTecnico uff1 = new UfficioTecnico();
+        final UfficioTecnico uff1 = new UfficioTecnico();
 
         uff1.setNome(aNome1);
         uff1.setEmail(aEmail1);
         uff1.setTel(aTel1);
         uff1.setUbicazione(aUbicazione1);
 
-        UfficioTecnico uff2 = new UfficioTecnico();
+        final UfficioTecnico uff2 = new UfficioTecnico();
 
         uff2.setNome(aNome2);
         uff2.setEmail(aEmail2);
         uff2.setTel(aTel2);
         uff2.setUbicazione(aUbicazione2);
-        List<UfficioTecnico> lista2 = new ArrayList<>();
+        final List<UfficioTecnico> lista2 = new ArrayList<>();
 
         lista2.add(uff1);
         lista2.add(uff2);
 
-        for (UfficioTecnico x : lista2) {
+        for (final UfficioTecnico x : lista2) {
             ufficioTecnicoDBTest.insert(x);
 
         }
 
-        List<UfficioTecnico> lista1 = ufficioTecnicoDBTest.getAll();
-        this.listEqualsIgnoreOrder(lista1, lista2);
+        final List<UfficioTecnico> lista1 = ufficioTecnicoDBTest.getAll();
         logger.info("return : " + this.listEqualsIgnoreOrder(lista1, lista2));
         System.out.println("lista 1 " + lista1 + "\n");
         System.out.println(lista2);
+        assertTrue(listEqualsIgnoreOrder(lista1, lista2));
     }
 
-    private <T> boolean listEqualsIgnoreOrder(List<T> list1, List<T> list2) {
+    /**
+     * List equals ignore order.
+     *
+     * @param       <T> the generic type
+     * @param list1 the list 1
+     * @param list2 the list 2
+     * @return true, if successful
+     */
+    private <T> boolean listEqualsIgnoreOrder(final List<T> list1,
+            final List<T> list2) {
         return new HashSet<>(list1).equals(new HashSet<>(list2));
     }
 
