5bff01d38ca534706a743a9e59ec2da6bd32c9d4
Andrea Mennillo <handymenny@outlook.com>
2019-01-25 11:29:17 +0100

    pool: Cleanup

--- src/main/java/pool/JDBCConnectionPool.java
+++ src/main/java/pool/JDBCConnectionPool.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-// TODO: Auto-generated Javadoc
+// TODO Auto-generated Javadoc
 /**
  * The Class JDBCConnectionPool.
  */
