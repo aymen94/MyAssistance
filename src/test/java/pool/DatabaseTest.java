a534509cf49a5be0da576a62ce9b31b51ef6966c
Gaetanosk8 <44467915+tanosk8@users.noreply.github.com>
2019-01-30 10:05:45 +0100

    DatabaseTest.java

---
+++ src/test/java/pool/DatabaseTest.java
@@ -0,0 +1,31 @@
+package pool;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+
+import static org.junit.Assert.*;
+
+public class DatabaseTest {
+
+    private  JDBCConnectionPool pool;
+    private Connection conn;
+
+    @Test public void initializePool() throws Exception {
+        Database.initializePool("databases.xml", "Test");
+        System.out.println(" --- Database initialized --- ");
+    }
+
+    @Test public void getConnection() throws Exception {
+        Database.initializePool("databases.xml", "Test");
+        conn = Database.getConnection();
+        System.out.println(" --- Database connected --- ");
+    }
+
+    @Test public void freeConnection() {
+        Database.initializePool("databases.xml", "Test");
+        conn = Database.getConnection();
+        Database.freeConnection(conn);
+        System.out.println(" --- Database FreeConnectioned --- ");
+    }
+}
